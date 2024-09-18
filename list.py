164b4a775050acde56088c43cc813a795ce4b0d8
Mallikharjuna71 <meka.mallikharjunareddy@diggibyte.com>
2024-09-18 15:35:02 +0530

    adding list file

---
+++ list.py
@@ -0,0 +1,11 @@
+'''
+LIST can store different types of objects
+
+insertion and deletion operations are simple
+
+indexing is possible
+
+it takes more memory and reading operation is slow compared to tuples
+
+
+'''
