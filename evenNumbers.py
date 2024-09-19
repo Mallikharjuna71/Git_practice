53b86ddbffb97f74f0d458e937d21475df7a9904
Mallikharjuna71 <meka.mallikharjunareddy@diggibyte.com>
2024-09-19 11:53:17 +0530

    adding deleted files

--- evenNumbers.py
+++
@@ -1,6 +0,0 @@
-def even_numbers(numbers):
-    for i in numbers:
-        if i%2==0:
-            print(i)
-
-even_numbers([1, 2,3, 4])
--- udf.py
+++
@@ -1,9 +0,0 @@
-def sum_of(a, b):
-    return a+b
-
-def greater(l):
-    great = l[0]
-    for i in l:
-        if i>great:
-            great = i
-    return great
